0d52756e7642b8ed57a298c0b720b62794946b0e
Andrew Gadner <reedkevin88@gmail.com>
2015-02-06 10:55:31 -0800

    Add initial stylesheets

---
+++ test/mailers/previews/vogogo_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/vogogo_mailer
+class VogogoMailerPreview < ActionMailer::Preview
+
+end
---
+++ test/mailers/vogogo_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class VogogoMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
